095c35144bb8ca8307aac6fa664cc6d8bf55ea62
Fernando <feranandonan@live.com>
2019-07-05 12:04:33 -0300

    [Codes] Melhorias no código
    Entre elas, a implementação de leitura de vídeo por arquivos, excluindo a necessidade da camera, visualização de quantos frames tem o vídeo e algumas funções comentadas(como o GrayScale, que é utilizado para ajudar na detecção facial)

--- codes/main.py
+++ codes/main.py
@@ -60,57 +60,6 @@ cv2.putText(img,'Hello World!',
     lineType)
 #############################
 
-
-#TESTING
-import cv2
-##QUICK BOOT
-video_capture = cv2.VideoCapture(0)
-if not video_capture.isOpened():
-    raise Exception("Could not open video device")
-ret, frame = video_capture.read()
-video_capture.release()
-
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10,30)
-fontScale              = 1
-fontColor              = (255,255,255)
-lineType               = 2
-
-proj_dir = "C:/NeoTokyo/Documents/GitHub/upgraded-eureka/codes/"
-face_cascade = cv2.CascadeClassifier(proj_dir+"haarcascade_frontalface_default.xml")
-
-##LOOP
-video_capture = cv2.VideoCapture(0)
-if not video_capture.isOpened():
-    raise Exception("Could not open video device")
-loops = 0;
-while (ret == True and loops<500):
-    ret, frame = video_capture.read()
-    #IMG PROCESSING    
-    cv2.putText(frame,'frame:'+str(loops), 
-        bottomLeftCornerOfText, 
-        font, 
-        fontScale,
-        fontColor,
-        lineType)
-
-    #Face recognition
-    faces = face_cascade.detectMultiScale(
-        frame,
-        scaleFactor=1.2,
-        minNeighbors=5
-        )
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    
-    cv2.imshow("Camera Frame",frame)
-    cv2.waitKey(1)
-    loops = loops + 1
-# Close device
-cv2.destroyAllWindows()
-video_capture.release()
-#FIM TESTE
-
 #rec = cv2.face.EigenFaceRecognizer_create()
 #rec = cv2.face.FisherFaceRecognizer_create()
 #rec = cv2.face.LBPHFaceRecognizer_create()
@@ -130,3 +79,95 @@ for (x, y, w, h) in faces:
 cv2.imshow("Window",gray_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+##########[GETTING CODE DIR]##################
+import os
+print(os.path)
+dirpath = os.getcwd()
+print("current directory is : " + dirpath)
+foldername = os.path.basename(dirpath)
+print("Directory name is : " + foldername)
+
+########################
+########################
+########################
+########################
+########################
+########################
+
+#Project Dir
+import os
+proj_dir = os.getcwd()
+proj_dir = proj_dir + "\\"
+#proj_dir = "C:/NeoTokyo/Documents/GitHub/upgraded-eureka/codes/"
+
+#VIDEO SOURCE, SET 0 to Camera
+#source = 0
+source = proj_dir+'video1.mp4'
+
+#TESTING
+import cv2
+##QUICK BOOT
+video_capture = cv2.VideoCapture(source)
+if not video_capture.isOpened():
+    raise Exception("Erro ao acessar fonte de vídeo")
+ret, frame = video_capture.read()
+video_capture.release()
+
+##TEXT DEFINITIONS
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+bottomLeftCornerOfText = (10,30)
+fontScale              = 1
+fontColor              = (255,255,255)
+lineType               = 2
+
+face_cascade = cv2.CascadeClassifier(proj_dir+"haarcascade_frontalface_default.xml")
+
+##LOOP
+video_capture = cv2.VideoCapture(source)
+if not video_capture.isOpened():
+    raise Exception("Erro ao acessar fonte de vídeo")
+
+loops = 0;
+length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT));
+
+while (ret == True and (loops<500 and loops != length-1)):
+    ret, frame = video_capture.read()
+    #IMG PROCESSING
+    ##IMG RESIZING - Lower Res = Higher Speed
+    size = 0.5;
+    frame = cv2.resize(frame,   (int(frame.shape[1]*size) , int(frame.shape[0]*size))   )
+    
+    ##INSERTING TEXT
+    cv2.putText(frame,'frame:'+str(loops)+' '+str(length), 
+        bottomLeftCornerOfText, 
+        font, 
+        fontScale,
+        fontColor,
+        lineType)
+
+    #Face recognition
+    ##Turning Gray (it wasnt used before and worked anyway)
+    ##frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    ##Recognizing
+    faces = face_cascade.detectMultiScale(
+        frame,
+        scaleFactor=1.2,
+        minNeighbors=5
+        )
+    #faces = faces*1/size
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv2.imshow("Camera Frame",frame)
+    #Uma segunda tela aumenta mais ou menos 2% a mais do processamento
+    #Mas pode servir para mostrarmos a imagem original e a usada para processar
+    #cv2.imshow("Camera Frame2",frame)
+    cv2.waitKey(1)
+    loops = loops + 1
+# Close device
+cv2.destroyAllWindows()
+video_capture.release()
+#FIM TESTE
+
